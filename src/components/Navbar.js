76b1f89469d4f5620a89767f030fc24e7ab54b4b
Tarek <lahouaouitarek99@gmail.com>
2022-09-09 16:28:53 +0100

    fix bug1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,13 +17,13 @@ export default function Navbar() {
 
             <ul className="socialMedia-list">
                 <li>
-                <a href="#"><AiFillFacebook size={"2rem"}/></a>
+                <a href="https://www.google.fr/"><AiFillFacebook size={"2rem"}/></a>
                 </li>
                 <li>
-                <a href="#"><AiFillTwitterSquare size={"2rem"}/></a>
+                <a href="https://www.google.fr/"><AiFillTwitterSquare size={"2rem"}/></a>
                 </li>
                 <li>
-                <a href="#"><AiFillInstagram size={"2rem"}/></a>
+                <a href="https://www.google.fr/"><AiFillInstagram size={"2rem"}/></a>
                 </li>
             </ul>
             <button className='btn-no-styiling' onClick={handleClick}>
@@ -33,10 +33,10 @@ export default function Navbar() {
         {
             Activate ?
             <div className="bg-white text-center border-bottom-2 p-0">
-                <a href="#" className="display-block p-20">About</a>
-                <a href="#" className="display-block p-20">Services</a>
-                <a href="#" className="display-block p-20">Support</a>
-                <a href="#" className="display-block p-20">Contact</a>
+                <a href="https://www.google.fr/" className="display-block p-20">About</a>
+                <a href="https://www.google.fr/" className="display-block p-20">Services</a>
+                <a href="https://www.google.fr/" className="display-block p-20">Support</a>
+                <a href="https://www.google.fr/" className="display-block p-20">Contact</a>
             </div>
                 :
             <></>
